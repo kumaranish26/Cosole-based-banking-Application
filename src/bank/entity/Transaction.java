0fc52d2c5568f1472326977bd7021e24e0a16411
Anish Kumar <kumaranish2605@gmail.com>
2026-04-02 23:38:25 +0530

    Fund transfer feature has been implemented

---
+++ src/bank/entity/Transaction.java
@@ -0,0 +1,72 @@
+package bank.entity;
+
+import java.time.LocalDate;
+
+public class Transaction {
+    private LocalDate transactionDate;
+    private String transactionUserId;
+    private Double transactionAmount;
+    private String transactionType;
+    private String initialBalance;
+    private String finalBalance;
+
+
+    public Transaction(LocalDate transactionDate, String transactionUserId, Double transactionAmount, String transactionType, String initialBalance, String finalBalance) {
+        this.transactionDate = transactionDate;
+        this.transactionUserId = transactionUserId;
+        this.transactionAmount = transactionAmount;
+        this.transactionType = transactionType;
+        this.initialBalance = initialBalance;
+        this.finalBalance = finalBalance;
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getTransactionUserId() {
+        return transactionUserId;
+    }
+
+    public void setTransactionUserId(String transactionUserId) {
+        this.transactionUserId = transactionUserId;
+    }
+
+    public Double getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(Double transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getInitialBalance() {
+        return initialBalance;
+    }
+
+    public void setInitialBalance(String initialBalance) {
+        this.initialBalance = initialBalance;
+    }
+
+    public String getFinalBalance() {
+        return finalBalance;
+    }
+
+    public void setFinalBalance(String finalBalance) {
+        this.finalBalance = finalBalance;
+    }
+
+
+}
