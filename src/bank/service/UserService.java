aa7b85718dcf82c139f298143b2a46c1a1851ed6
Anish Kumar <kumaranish2605@gmail.com>
2026-05-12 20:09:21 +0530

    Printing Transaction to admin console feature is added

--- src/bank/service/UserService.java
+++ src/bank/service/UserService.java
@@ -32,9 +32,9 @@ public class UserService {
           UserRepository userRepository=new UserRepository();
           userRepository.printTransactions(userId);
     }
-    public void checkIssue(String username,String password,String contact)
-    {
-
-    }
+//    public void checkIssue(String username,String password,String contact)
+//    {
+//
+//    }
 }
 
